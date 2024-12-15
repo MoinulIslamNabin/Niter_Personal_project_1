34b82f816c90eb416484d0e5edebd898d15497f5
MoinulIslamNabin <moinul.islam.nabin779@gmail.com>
2024-12-15 20:26:06 +0600

    logo title  changed and button modified

--- src/Components/Home/Docs/Docs.jsx
+++ src/Components/Home/Docs/Docs.jsx
@@ -5,9 +5,9 @@ const Docs = () => {
     return (
         <>
         <div className='h-screen py-8 flex gap-4 justify-center items-center'>
-            <Link to={"/notes"} className="btn bg-[#b9c6ae] text-black glass">Class Note</Link>
-            <Link to={"/books"} className="btn bg-[#b9c6ae] text-black glass">Books</Link>
-            <Link to={"/questions"} className="btn bg-[#b9c6ae] text-black glass">Exam Questions</Link>
+            <Link to={"/notes"} className="btn bg-[#b9c6ae] text-black hover:text-white glass">Class Note</Link>
+            <Link to={"/books"} className="btn bg-[#b9c6ae] text-black hover:text-white glass">Books</Link>
+            <Link to={"/questions"} className="btn bg-[#b9c6ae] text-black hover:text-white glass">Exam Questions</Link>
         </div>
         
         </>
